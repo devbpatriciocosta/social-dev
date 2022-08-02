97a687a328b3a6be634b1083e74cd3c602ee350e
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-02 11:03:36 -0300

    Criação do componente dde imagem + Início da Login

---
+++ pages/login.jsx
@@ -0,0 +1,13 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+function LoginPage () {
+  return (
+    
+      <ImageWithSpace>
+        Teste 123
+      </ImageWithSpace>
+    
+  )
+}
+
+export default LoginPage
