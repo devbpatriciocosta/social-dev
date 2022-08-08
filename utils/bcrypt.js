4a043e12954ee949e6041225b295fad1899780c9
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-08 10:40:01 -0300

    Criado model de user

---
+++ utils/bcrypt.js
@@ -0,0 +1,9 @@
+import bcryptjs from 'bcryptjs'
+
+
+export const hashPassword = (password) => 
+  bcryptjs.hashSync(password)
+
+  export const comparePassword = (password, hash) => 
+    bcryptjs.compareSync(password, hash)
+
