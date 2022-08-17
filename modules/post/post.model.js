035bf1d5f48def9d2dc82fca7be974ccf79d2795
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-17 18:19:28 -0300

    Correção de bug que eu mesmo criei

--- modules/post/post.model.js
+++ modules/post/post.model.js
@@ -5,7 +5,7 @@ import User from '../user/user.model'
 const PostSchema = new mongoose.Schema({
   text: { type: String, required: true, maxlength: 256 },
   createdDate: { type: Date, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user' }
+  createdBy: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }
 })
 
 export default mongoose.models.Post || mongoose.model('Post', PostSchema)
