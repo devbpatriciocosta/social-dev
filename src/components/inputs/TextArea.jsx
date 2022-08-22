b47501ccc8d237c14437e64fba2e90859e7bccc6
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-22 11:59:34 -0300

    2

--- src/components/inputs/TextArea.jsx
+++ src/components/inputs/TextArea.jsx
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
-const Textarea = styled.textarea`
+const TextArea = styled.textarea`
   resize: none;
   width: 100%;
   padding: 15px 20px;
   box-sizing: border-box;
   border: 1px solid ${(props) => props.theme.inputBorder};
   border-radius: 10px;
-  background-color: ${(props) =>
-    props.theme.inputBackground};
+  background-color: ${(props) => props.theme.inputBackground};
   font-family: 'Roboto', sans-serif;
   font-size: 14px;
 `;
 
-export default Textarea;
+export default TextArea;
